220279c04c01e38c12fd21b08287b538c8c5ff7a
Liden <lninyow@gmail.com>
2023-10-10 16:29:58 +0800

    test

--- src/First.js
+++ src/First.js
@@ -2,20 +2,17 @@ import React, { useState } from "react";
 import { useOutletContext } from "react-router-dom";
 
 export default function First() {
-  const [inputValue, setInputValue] = useState(""); 
   const [lyrics, setLyrics] = useOutletContext();
 
   const updateInput = (e) => {
     const newLyric = e.target.value;
-    setInputValue(newLyric); 
     setLyrics(newLyric); 
   };
 
   return (
     <input
-      style={{ width: 900, height: 40, borderRadius: 20,height:75}}
+      style={{ width: 900, borderRadius: 20,height:75}}
       type="text"
-      value={inputValue}
       onInput={updateInput}
     />
   );
--- src/Second.js
+++ src/Second.js
@@ -2,20 +2,17 @@ import React, { useState } from "react";
 import { useOutletContext } from "react-router-dom";
 
 export default function Second() {
-  const [inputValue, setInputValue] = useState(""); // New state to hold input value
   const [lyrics, setLyrics] = useOutletContext();
 
   const updateInput = (e) => {
     const newLyric = e.target.value;
-    setInputValue(newLyric); // Update the input value
-    setLyrics(newLyric); // Pass the new lyric as context
+    setLyrics(newLyric);
   };
 
   return (
     <input
-    style={{ width: 900, height: 40, borderRadius: 20,height:75}}
+    style={{ width: 900, borderRadius: 20,height:75}}
     type="text"
-      value={inputValue}
       onInput={updateInput}
     />
   );
